991d624add74cfbaa1336cacacc446d65d2fc20a
Priyanshu-20025025 <priyanshusharma135a@gmail.com>
2024-10-21 17:49:54 +0530

    landing page why choose us created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 
 import './App.css'
+import ContactUs from './components/ContactUs'
+import LandingPage from './components/LandingPage/LandingPage'
+import WhyChooseUs from './components/WhyChosseUs/WhyChooseUs'
 import Header from './pages/Header/Header'
 
 function App() {
   
 
   return (
-    <>
+    <div className='h-full w-full'>
       <Header/>
-    </>
+      <LandingPage/>
+      <WhyChooseUs/>
+      <ContactUs/>
+    </div>
   )
 }
 
