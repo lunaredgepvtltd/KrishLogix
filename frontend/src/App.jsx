e59c56f2d889bdd5e13a3eb8c8213628a24fa3a6
sujeet3008 <ranjansujeet34@gmail.com>
2024-10-21 17:52:22 +0530

    ourteam,contact us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,27 @@
 
 import './App.css'
 import Header from './pages/Header/Header'
+import OurTeam from './components/OurTeam/OurTeam'
+import ContactUs from './components/ContactUs/ContactUs'
+import Footer from './pages/Footer/Footer.jsx'
 
 function App() {
   
 
   return (
-    <>
-      <Header/>
-    </>
+   <div className="h-full w-full"> 
+   
+    <Header/>
+       <OurTeam /> 
+      <ContactUs /> 
+
+      <Footer />
+      
+
+
+   </div>
+      
+   
   )
 }
 
--- frontend/src/components/OurTeam/OurTeam.jsx
+++ frontend/src/components/OurTeam/OurTeam.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
-import "./OurTeam.png"
+import React from "react";
+import teamBgImage from "./OurTeam.png";
 
 const OurTeam = () => {
   return (
-    <div className=''>
-      hello
+    <div
+      className="relative h-[80vh] w-full bg-cover bg-center"
+      style={{ backgroundImage: `url(${teamBgImage})` }}
+    >
+      
 
+      <div className="relative flex flex-col justify-center items-center h-full text-black px-4">
+        <h1 className="text-4xl  md:text-6xl font-bold mb-8 text-center ">
+          OUR TEAM
+        </h1>
+
+        <p className="text-sm md:text-xl text-left mb-8 w-[85%]">
+          Our team is dedicated to ensuring that your freight forwarding,
+          transportation, and logistics needs are met with the utmost precision
+          and care. Whether it’s optimizing routes, managing shipments, or
+          solving logistical challenges, we’re committed to delivering solutions
+          that drive your success. Let’s work together to make your logistics
+          experience as smooth and efficient as possible!
+        </p>
+
+        <p className="text-sm md:text-xl text-left w-[85%]">
+          We specialize in providing seamless, efficient, and reliable solutions
+          to meet all your supply chain needs.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OurTeam
+export default OurTeam;
